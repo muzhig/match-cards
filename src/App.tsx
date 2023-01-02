b05512ef6a4bfaa3a616a6e8a3fc57b63f21f39b
Arseniy Potapov <arseniy@potapov.dev>
2023-01-02 15:11:17 +0600

    fix flower1

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,7 @@ function App() {
     {words: ['milk'], pictures: ['milk-1.png', 'milk-2.png', 'milk-3.png']},
     {words: ['pizza'], pictures: ['pizza.png', 'pizza-2.png']},
     {words: ['potato'], pictures: ['potato.png', 'potato-2.png', 'potato-1672572315.png']},
-    // 1..15
-    {words: ['flower'], pictures: ['flower.png', 'flower-2.png', 'flower-3.png', 'flower-4.png', 'flower-5.png',
+    {words: ['flower'], pictures: ['flower-1.png', 'flower-2.png', 'flower-3.png', 'flower-4.png', 'flower-5.png',
         'flower-6.png', 'flower-7.png', 'flower-8.png', 'flower-9.png', 'flower-10.png', 'flower-11.png', 'flower-12.png',
         'flower-13.png', 'flower-14.png', 'flower-15.png']},
 
