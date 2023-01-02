cd11350cdcd93e602b337cffb8c645e171328670
Arseniy Potapov <arseniy@potapov.dev>
2023-01-03 00:18:28 +0600

    more images, removed labels, increased grid

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,18 @@ function intersection<T>(a: Array<T>, b: Array<T>): Array<T> {
   return a.filter((x) => b.includes(x));
 }
 
+const speak = (word: string) => {
+    const voices = speechSynthesis.getVoices();
+    const voice = voices.find((v) => v.name === 'Google US English');
+    const utterance = new SpeechSynthesisUtterance(word);
+    utterance.voice = voice || null;
+    utterance.pitch = 1.0 + Math.random() * 0.2;
+    utterance.rate = 0.7 + Math.random() * 0.3;
+    utterance.lang = 'en-US'
+
+    speechSynthesis.speak(utterance);
+  };
+
 function App() {
   const wordsPairs = [
     {words: ['car'], pictures: ['car-1.png', 'car-2.png', 'car-3.png', 'car-4.png']},
@@ -52,7 +64,7 @@ function App() {
     {words: ['chair'], pictures: ['chair.png', 'chair-2.png']},
     {words: ['chicken'], pictures: ['chicken-1672572083.png', 'chicken-1672572060.png']},
     {words: ['elephant'], pictures: ['elephant.png', 'elephant-2.png']},
-    {words: ['fish'], pictures: ['fish-1672641718.png']},
+    {words: ['fish'], pictures: ['fish-1672641718.png', 'fish-2.png', 'fish-3.png', 'fish-4.png', 'fish-5.png']},
     {words: ['meat'], pictures: ['meat.png', 'meat-1672572304.png']},
     {words: ['milk'], pictures: ['milk-1.png', 'milk-2.png', 'milk-3.png']},
     {words: ['pizza'], pictures: ['pizza.png', 'pizza-2.png']},
@@ -60,14 +72,39 @@ function App() {
     {words: ['flower'], pictures: ['flower-1.png', 'flower-2.png', 'flower-3.png', 'flower-4.png', 'flower-5.png',
         'flower-7.png', 'flower-8.png', 'flower-9.png', 'flower-10.png', 'flower-11.png', 'flower-12.png',
         'flower-13.png', 'flower-14.png', 'flower-15.png']},
+    {words: ['yellow'], pictures: [], color: 'yellow'},
+    {words: ['red'], pictures: [], color: 'red'},
+    {words: ['blue'], pictures: [], color: 'blue'},
+    {words: ['green'], pictures: [], color: 'green'},
+    {words: ['orange'], pictures: [], color: 'orange'},
+    // {words: ['white'], pictures: [], color: 'white'},
+    {words: ['black'], pictures: [], color: 'black'},
+    {words: ['purple'], pictures: [], color: 'purple'},
+    {words: ['brown'], pictures: [], color: 'brown'},
+    {words: ['pink'], pictures: [], color: 'pink'},
+    {words: ['gray'], pictures: [], color: 'gray'},
+
+    {words: ['zero'], pictures: [], glyph: '0'},
+    {words: ['one'], pictures: [], glyph: '1'},
+    {words: ['two'], pictures: [], glyph: '2'},
+    {words: ['three'], pictures: [], glyph: '3'},
+    {words: ['four'], pictures: [], glyph: '4'},
+    {words: ['five'], pictures: [], glyph: '5'},
+    {words: ['six'], pictures: [], glyph: '6'},
+    {words: ['seven'], pictures: [], glyph: '7'},
+    {words: ['eight'], pictures: [], glyph: '8'},
+    {words: ['nine'], pictures: [], glyph: '9'},
+
+    {words: ['smile'], pictures: [], glyph: '😀'},
 
   ]
-  const gridSideSize = 4;
-  const gridSize = gridSideSize * gridSideSize;
+  const gridWidth = 6;
+  const gridHeight = 5;
+  const gridSize = gridWidth * gridHeight;
   const selectedWordsPairs = shuffle(wordsPairs).slice(0, Math.floor(gridSize / 2));  // 8 words
   const cardDefs = selectedWordsPairs.map((pair, i) => [
     {id: `word-${i}`, word: choice(pair.words), picture: undefined, src: pair},
-    {id: `pic-${i}`, picture: choice(pair.pictures), src: pair},
+    {id: `pic-${i}`, picture: choice(pair.pictures), color: pair.color, glyph: pair.glyph, src: pair},
   ]);
   const cardsArray: Array<CardState> = shuffle(cardDefs[0].concat(...cardDefs.slice(1)).map((card, i) => (
     {
@@ -77,22 +114,8 @@ function App() {
   // shuffled
   const [cards, setCards] = useState(cardsArray);
   const [selected, setSelected] = useState<CardState | undefined>(undefined);
-  const grid = []
-  const rowSize = 4
-  for (let i = 0; i < cards.length; i += rowSize) {
-    grid.push(cards.slice(i, i + rowSize))
-  }
-  const speak = (word: string) => {
-    const voices = speechSynthesis.getVoices();
-    const voice = voices.find((v) => v.name === 'Google US English');
-    const utterance = new SpeechSynthesisUtterance(word);
-    utterance.voice = voice || null;
-    utterance.pitch = 1.0 + Math.random() * 0.2;
-    utterance.rate = 0.7 + Math.random() * 0.3;
-    utterance.lang = 'en-US'
+  const grid = chunks(cards, gridWidth);
 
-    speechSynthesis.speak(utterance);
-  };
   return (
     <div className="App">
       {
@@ -103,8 +126,8 @@ function App() {
                 <Card
                   card={card}
                   key={card.id}
-                  className={`${card.selected ? 'selected' : ''} ${card.hidden ? 'hidden' : ''} ${card.shake ? 'shake' : ''}`}
-
+                  className={`${card.selected ? 'selected' : ''} ${card.hidden ? 'hidden' : ''} ${card.shake ? 'shake' : ''} ${card.src.props?.className || ''}`}
+                  style={card.src.props?.css||{}}
                   onClick={() => {
                     if (selected && selected.id !== card.id) {
                       if (intersection(selected.src.words, card.src.words).length > 0) {
